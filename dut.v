1937896787343b80c0acbb96b943dcbe4346cc02
Wilber Hernandez <11430410+wilberahr@users.noreply.github.com>
2024-10-05 18:45:46 -0600

    Programación del estado "comparar pin". Se agregan comentarios en los end para ver donde terminan los condicionales.

--- dut.v
+++ dut.v
@@ -39,6 +39,9 @@ module cajero(  clock,
     reg [1:0] cuenta_pin_bit;
     reg [1:0] proxima_cuenta_pin_bit;
 
+    reg [1:0] intentos_fallidos;
+    reg [1:0] proximo_intentos_fallidos;
+
     reg [15:0] pin_recibido;
 
     integer cuenta_digito_bit;
@@ -69,10 +72,12 @@ module cajero(  clock,
         if (reset) begin 
             estado_actual <= 5'b00001; 
             cuenta_pin_bit <= 2'b00;
+            intentos_fallidos <= 2'b00;
         end //end if
         else begin
             estado_actual <= proximo_estado;
             cuenta_pin_bit <= proxima_cuenta_pin_bit;
+            intentos_fallidos <= proximo_intentos_fallidos;
         end //end else        
     end //end always @(posedge clock)
 
@@ -81,6 +86,7 @@ module cajero(  clock,
 
         estado_actual = proximo_estado;
         cuenta_pin_bit = proxima_cuenta_pin_bit;
+        intentos_fallidos = proximo_intentos_fallidos;
 
         case (estado_actual)
 
@@ -88,11 +94,11 @@ module cajero(  clock,
                 begin
                     if(tarjeta_recibida == 1) begin
                         proximo_estado = recibiendo_pin;
-                    end //end if
+                    end // if
                     else begin
                         proximo_estado = idle;
-                    end //end else
-                end //end idle
+                    end // else
+                end // idle
 
             recibiendo_pin:
                 begin
@@ -100,28 +106,57 @@ module cajero(  clock,
                     if(cuenta_pin_bit < 16) begin
                         if(digito_stb == 1'b0) begin
                             proximo_estado = recibiendo_pin;
-                        end //end if
+                        end //end if(digito_stb == 1'b0)
                         else begin
                             //Se recorre el digito entrante y se asigna al bit al registo de pin recibido, con los digitos anteriores
                             for(cuenta_digito_bit = 0; cuenta_digito_bit<4; cuenta_digito_bit = cuenta_digito_bit + 1) begin
                                 pin_recibido[cuenta_pin_bit + cuenta_digito_bit] = digito[cuenta_digito_bit];
-                            end
+                            end //end for
                             //Se suma 4 bits a la cuenta de bits en el pin
                             proxima_cuenta_pin_bit = cuenta_pin_bit + 4;
                             proximo_estado = recibiendo_pin;
 
                         
-                        end //end else
-                    end
+                        end // else
+                    end // if(cuenta_pin_bit < 16)
                     //Si ya se ingreso todo el pin
                     else begin
                         //Se reinicia el contador de digitos
                         proxima_cuenta_pin_bit = 0;
                         proximo_estado = comparar_pin;
-                    end
+                    end //else
 
-                end//end recibiendo_pin
+                end // recibiendo_pin
 
+            comparar_pin:
+                begin
+                    // Se ingreso el pin correcto
+                    if(pin_recibido == pin) begin
+                        proximo_intentos_fallidos = 2'b00;
+                        proximo_estado = transaccion;
+                    end //end if
+                    else begin
+                        //Si no han habido intentos fallidos
+                        if(intentos_fallidos == 2'b00) begin
+                            pin_incorrecto = 1'b1;
+                            proximo_intentos_fallidos = intentos_fallidos + 1;
+                            proximo_estado = transaccion;
+                        end //end if
+                        // Si ha habido un intento fallido
+                        else if(intentos_fallidos == 2'b10) begin
+                            advertencia = 1'b1;
+                            proximo_intentos_fallidos = intentos_fallidos + 1;
+                            proximo_estado = transaccion;
+                        end //end else if
+                        // Si ha habido dos intentos fallidos
+                        else if(intentos_fallidos == 2'b11) begin
+                            advertencia = 1'b0;
+                            bloqueo = 1'b1;
+                            proximo_intentos_fallidos = 2'b00;
+                            proximo_estado = bloqueo_cajero;
+                        end //end else if
+                    end //end else
+                end //comparar_pin
         endcase
     end //end always @(*)
 
