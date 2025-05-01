7dcbe9a9a3aad46dc9810173ab44223284d1f0f0
Wilber Hernandez <11430410+wilberahr@users.noreply.github.com>
2025-05-01 16:17:32 -0600

    Se agrega la instanciación del tester para el estado "idle".

--- test/idle/testbench.v
+++ test/idle/testbench.v
@@ -15,14 +15,22 @@ module testbench;
         .tarjeta_recibida(tarjeta_recibida),
         .fin(fin)
     );
+    
+    // Instanciacion del tester
+    tester TESTER (
+        .clk(clk),
+        .reset(reset),
+        .tarjeta_recibida(tarjeta_recibida),
+        .fin(fin)
+    );
 
     // Resultados de la simulacion
     initial begin
         $dumpfile("resultados.vcd");
         $dumpvars(-1,testbench);
         $monitor(
-            "Tiempo: %0t | Reset: %b | Input: %b | Output: %b", 
+            "Tiempo: %0t | Reset: %b | tarjeta_recibida: %b | fin: %b", 
             $time, reset, tarjeta_recibida,fin);
-     end
+    end
 
 endmodule
